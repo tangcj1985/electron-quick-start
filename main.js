52fe681242a72f79fb9c9808963c0b458f769bc1
tangcj1985 <373032526@qq.com>
2018-12-02 15:19:41 +0800

    open remote url and use node.js modules

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {
+  app,
+  BrowserWindow
+} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  // mainWindow.loadFile('index.html')
+  // mainWindow.loadURL('http://localhost:8888/electron/index.html')
+  mainWindow.loadURL('http://localhost:8080')
+  mainWindow.webContents.executeJavaScript(`
+    let basePath = process.cwd();
+
+    window.Qbian = require(basePath + '//resources//app//index.js');
+    console.info('--executeJavaScript export Object --> ', window.Qbian);
+`);
+
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
